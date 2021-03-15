3ba09c57fd57822f847b4cc4adbaa27595f14de9
thomas <thomascoles14@gmail.com>
2021-03-14 20:17:54 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(name){
+    return "Hi, my name is name.";
+}
+function introductionWithLanguage(name, language) {
+    console.log("Hi, my name is name and I am learning to program in language.")
+}
+function introductionWithLanguageOptional(name,language=Javascript) {
+    console.log(javascript)
+}
